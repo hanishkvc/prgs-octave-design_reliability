5cef4e9d3b29a3b1e07431b3a1818629985b8e09
hanishkvc <hanishkvc@gmail.com>
2016-10-18 23:59:59 +0530

    Added logic to specify a product quantity and time to check
    Now added logic which asks user to enter
    
    a custom product quantity and
    a custom amount of time at which to check failure
    
    In turn this is added to the array of quantities and times
    so that we get failed quantity of units for the given quantity
    of products at the time specified.

--- mtbf_failurecalc.m
+++ mtbf_failurecalc.m
@@ -1,5 +1,9 @@
 # Calculate MTBF Related info
 # Variables maintain info in hours
+# v20161018_2008, HanishKVC
+#
+
+disp("INFO: Enter all time in hours")
 
 function print_times(msg, timeInHours)
   printf(msg)
@@ -9,8 +13,22 @@ function print_times(msg, timeInHours)
     timeInHours, timeInDays, timeInYears)
 endfunction
 
+function matR = addto_1Dmatrix(matI, newData)
+  dataSize = size(newData);
+  if ((dataSize(1) == 0) && (dataSize(2) == 0))
+    disp("WARN:addto: Empty newData")
+  else
+    index = size(matI)(2);
+    matI(index+1) = newData;
+  endif
+  matR = matI;
+endfunction
+
 mtbf = input("Enter MTBF of product in hours: ");
 print_times("\nProduct MTBF", mtbf)
+
+time2Check = input("Check Failure at time:");
+qty2Check = input("Quantity of Product in field:");
 
 totalTime = max(mtbf,10*365*24);
 print_times("\nTotal time considered", totalTime)
@@ -21,10 +39,14 @@ Reliability = e.^(-timeX/mtbf);
 plot(timeX,Reliability)
 
 
-timeConsidered = [24*365, 24*365*2, 24*365*3, 24*365*5, 24*365*6, 24*365*8, 24*365*10];
+#timeConsidered = [24*365, 24*365*2, 24*365*3, 24*365*5, 24*365*6, 24*365*8, 24*365*10];
+#timeConsidered = [24*365, 24*365*2, 24*365*3, 24*365*5, 24*365*6];
+timeConsidered = [24*365, 24*365*2, 24*365*3, 24*365*5];
+timeConsidered = addto_1Dmatrix(timeConsidered, time2Check);
 prodQuantities = [10, 50, 100, 1000, 5000, 10000];
+prodQuantities = addto_1Dmatrix(prodQuantities, qty2Check);
 
-disp("\nAssuming 24x7 i.e Continuous use of the product in the field")
+printf("\nAssuming 24x7 i.e Continuous use of the product (with %d MTBF) in the field\n", mtbf)
 
 for curTime = timeConsidered
   curTimeInYears = curTime/(24*365);
